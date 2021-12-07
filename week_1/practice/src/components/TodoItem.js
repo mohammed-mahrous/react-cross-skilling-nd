bc6197dfb20afd88116661e2bb99ee6bc5a2be55
mohammed-mahrous <xxspider6@gmail.com>
2021-12-08 00:18:53 +0200

    add checkbox

--- week_1/practice/src/components/TodoItem.js
+++ week_1/practice/src/components/TodoItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 class TodoItem extends React.Component {
   render() {
@@ -14,7 +14,9 @@ class TodoItem extends React.Component {
             1. Add a checkbox to the todo item
             2. The checkbox should be checked if the todo is completed (completed === true)
         */}
-        <div>{completed.toString()}</div>
+        <div>
+          <input type="checkbox" checked={completed} />
+        </div>
       </li>
     );
   }
--- week_1/practice/src/components/TodoList.js
+++ week_1/practice/src/components/TodoList.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import TodoItem from './TodoItem';
+import React, { Component } from "react";
+import TodoItem from "./TodoItem";
 
 //we will use a static todo list for now
 const TodoListItems = [
   {
     id: 1,
-    text: 'Learn React',
+    text: "Learn React",
     completed: false,
   },
   {
     id: 2,
-    text: 'Learn JS',
+    text: "Learn JS",
     completed: false,
   },
   {
     id: 3,
-    text: 'Learn Anything',
+    text: "Learn Anything",
     completed: true,
   },
 ];
 class TodoList extends Component {
   render() {
     return (
-      <ol className='todo-list-container'>
+      <ol className="todo-list-container">
         {TodoListItems.map((item) => (
           <TodoItem todo={item} key={item.id} />
         ))}
